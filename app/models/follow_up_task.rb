b893cffc3f2c74cb3eb8fa3936bd2404a41269d5
ntxtthomas <apm.tthomas@gmail.com>
2026-04-27 16:43:58 -0500

    fix enum syntax, fix rubocop

--- app/models/follow_up_task.rb
+++ app/models/follow_up_task.rb
@@ -1,5 +1,5 @@
 class FollowUpTask < ApplicationRecord
   belongs_to :prospect_id
 
-  enum priority: { low: 0, medium: 1, high: 2 }
+  enum :priority { low: 0, medium: 1, high: 2 }
 end
--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,3 +1,3 @@
 class Prospect < ApplicationRecord
-    enum status: { new: 0, contacted: 1, tour_scheduled: 2, applied: 3, moved_in: 4, closed_lost: 5}
+    enum :status { new: 0, contacted: 1, tour_scheduled: 2, applied: 3, moved_in: 4, closed_lost: 5 }
 end
